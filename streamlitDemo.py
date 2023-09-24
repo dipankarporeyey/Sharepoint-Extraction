29dbec91ab9faa17c055f358ffd0efcc52f6c8ba
Dipankar Porey <142393645+dipankarporeyey@users.noreply.github.com>
2023-09-24 12:39:40 +0530

    Update streamlitDemo.py

--- streamlitDemo.py
+++ streamlitDemo.py
@@ -1,70 +1,53 @@
-# import os
+import time
+# import numpy as np
 from SharePointToLocalSystem import sharePointToLocalSystem
 from CreateCredential import createCredential
 import streamlit as st
 
+
 # Header
-#######################################
-st.sidebar.title("Asking for credential")
-
-# Text Input
-client_id = st.sidebar.text_input("Enter Client ID")
-
-# Text Input
-client_secret = st.sidebar.text_input("Enter Client Secret Code")
-
-# Text Input
-datafile_link = st.sidebar.text_input("Enter File Link")
-local_file_name, dataset_url, site_url = createCredential.detailsCredential(datafile_link)
-
-# Text Input
-# local_file_path = os.getcwd() # st.text_input("Enter Client ID")
-if st.sidebar.button("Click here to submit"):
-    st.sidebar.success("Checking . . .", icon="✅")
-##########################################
-
 st.title("Sharepoint Extraction Process!🦜")
 
-# if st.button("Submit"):
-with st.spinner("File extraction in progress..."):
+# Main
+#######################################
+# st.write("Asking for credentials")
+# st.caption('Asking for credentials :blue[colors] and emojis :sunglasses:')
+st.caption(':blue[Asking for credentials]')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    # Text Input: Client ID
+    client_id = st.text_input("Enter Client ID")
+with col2:
+    # Text Input: Client Sceret Code
+    client_secret = st.text_input("Enter Client Secret Code")
+with col3:
+    # Text Input: File Link
+    datafile_link = st.text_input("Enter File Link")
+    
+local_file_name, dataset_url, site_url = createCredential.detailsCredential(datafile_link)
+
+if st.button("Click here to submit"):
     try:
-        response = sharePointToLocalSystem.SaveLocalSystem(client_id, client_secret, site_url, dataset_url)
         with st.status("Downloading data . . .", expanded=True) as status:
-            st.write("Searching for data . . .")
+            st.caption(":blue[Searching for data . . .]")
             time.sleep(2)
-            st.write("Found URL !")
+            response = sharePointToLocalSystem.SaveLocalSystem(client_id, client_secret, site_url, dataset_url)
+            st.caption(":red[Found URL !]")
             time.sleep(1)
-            st.write("Downloading data . . .")
+            st.caption(":green[Downloading data . . .]")
             time.sleep(1)
-            st.download_button(label="Click here to download!",
-                               data=response,
-                               file_name=local_file_name,
-                               key='download-button'
-                              )
+            st.success("File extraction successful✅!")
             status.update(label="Download complete!", state="complete", expanded=True)
-        
-        st.button('Rerun')
-        col1, col2 = st.columns(1)
+        col1, col2 = st.columns(2)
         with col1:
-            st.write("File extraction successfully!")
+            st.caption(":blue[Received file name:]  "+str(local_file_name))
         with col2:
-            st.write("Received file name:  "+str(local_file_name))
-        # with col1:
-        #     st.write("Click to download the received file!")
-        # Create a download button
-        # csv_data = df.to_csv(index=False).encode()
-        # with col2:
-        #     st.download_button(label="Download file",
-        #                     data=response,
-        #                     file_name=local_file_name,
-        #                     key='download-button'
-        #                     )
-        # st.write("Please find the required file in the local path:  "+str(local_file_path))
-        # st.write(local_file_path)
+            st.download_button(label="Click here to download!",
+                                data=response,
+                                file_name=local_file_name,
+                                key='download-button'
+                                )
+        status.update(label="Download complete!", state="complete", expanded=True)
     except Exception as e:
-        st.write("Please enter correct credentials ⚠️. Thank you!")
-# st.write("Received File: "+str(local_file_name))
-
-
-
-
+        st.warning("Please enter correct credentials!", icon="⚠️")
